5b598f964382774cd1cd7085dfb6ea97930c562a
RegulaHuber <regula.huber@stud.sfgz.ch>
2025-10-29 21:44:18 +0100

    update lernjournal

--- aufgabe_01/sketch.js
+++ aufgabe_01/sketch.js
@@ -1,22 +1,27 @@
 let valueSlider;  // hier ausserhalb von setup klebt es in der Javascript-Welt // globale Variable
 let newValueSlider;
+let thirdValueSlider;
+
+let farbwert;
+farbwert = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // erster Slider
 
-valueSlider = createSlider(0, 400, 400); // min, max, start 
+valueSlider = createSlider(0, 400, 0); // min, max, start 
 valueSlider.position(width/2-70,height/2-70,);
 
 // zweiter Slider
 
-newValueSlider = createSlider(0, 400, 400); // min, max, start
+newValueSlider = createSlider(0, 400, 0); // min, max, start
 newValueSlider.position(width/2-70,height/2-10,);
 
 // dritter Slider
 
-thirdValueSlider = createSlider(0, 400, 400); // min, max, start
+thirdValueSlider = createSlider(0, 400, 0); // min, max, start
 thirdValueSlider.position(width/2-70,height/2+50,);
 
 
@@ -49,23 +54,39 @@ let newOutputMax = 20;// max Wert
 let newOutputValue= map(newInputValue, newInputMin, newInputMax, newOutputMin, newOutputMax); // umrechnen von einem Bereich in einen anderen Bereich
 console.log(newOutputValue); // 
 
+// Wert vom dritten Slider holen und umrechnen (Position von 0 bis 100)
+
+let thirdInputValue = thirdValueSlider.value(); // 0 bis 400
+let thirdInputMin= 0; // min Wert
+let thirdInputMax = 400; // max Wert
+
+let thirdOutputMin = 20; // min Wert
+let thirdOutputMax = 255;// max Wert
+
+let thirdOutputValue= map(thirdInputValue, thirdInputMin, thirdInputMax, thirdOutputMin, thirdOutputMax); // umrechnen von einem Bereich in einen anderen Bereich
+console.log(thirdOutputValue); //
+
+
+
 
 
   background(50);
 
 
-  fill(255-outputValue,); // Kreisfarbe von weiss zu schwarz je nach Mausposition
+  fill(255-outputValue,thirdOutputValue, 120); // Kreisfarbe von weiss zu schwarz je nach Mausposition
   strokeWeight(0+newOutputValue);
   stroke(200,0,200)
 
-  ellipse(width/2,height/2,300,300);
-  arc(width/2,height/2-250, 100, 100, 120, PI + HALF_PI);
-  triangle(width/2, height/2+100, width/2+280, height/2+280, width/2-280, height/2+280);
-
-
-
-
-
-
+  ellipse(width/2,height/2+outputValue-250,250,250);
+  arc(width/2-200,height/2-200, outputValue/2, outputValue/2, 120, PI + HALF_PI);
+  triangle(width/2+outputValue-100, height/2+100, width/2+280, height/2+280, width/2-280, height/2+280);
 
 }
+
+
+function mouseClicked() {
+  mauswert = mauswert + 40; 
+  console.log(mauswert);
+
+
+}// bei Klick erhöhen 
