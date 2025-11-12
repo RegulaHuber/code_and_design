78ccb5286c216e47f587b10b8dc9100340e38dd6
RegulaHuber <regula.huber@stud.sfgz.ch>
2025-11-12 22:25:19 +0100

    update fish und sound

--- aufgabe_02/sketch.js
+++ aufgabe_02/sketch.js
@@ -1,63 +1,92 @@
-let size = 100; // Startgrösse
+let size = 100; // Startgrösse der Katze
+
+let sound;
+
+function preload() {
+  sound = loadSound('cat.mp3');
+}
+
+function draw() {}
+
+function mousePressed() {
+  sound.play();
+}
+
 
 let stars = [];
 
+const originStars = {x: 200, y: 200};
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+    noCursor(); // Versteckt den normalen Mauszeiger
     fill(100,100,100);
-
-    
+  
+    const radius = (width ** 2 + height ** 2) ** 0.5 * 0.7;
+  
     // Sternenhimmel
-for (let i = 0; i < 300; i++) {         // Anzahl Sterne 
-    stars.push({                        // Objekt für jeden Stern
-      x: random(width),                 // Zufällige Position
-      y: random(height),                
-      size: random(1, 3),               // Zufällige Grösse
-      baseBrightness: random(100, 255), // Grundhelligkeit
-      twinkleSpeed: random(0.01, 0.1),  // Funkelgeschwindigkeit
+    for (let i = 0; i < 500; i++) {                  // Anzahl Sterne 
+    stars.push({                                      // Objekt für jeden Stern
+      x: random(radius * 2) - radius + originStars.x, // Zufällige Position
+      y: random(radius * 2) - radius + originStars.y,                
+      size: random(2, 5),                             // Zufällige Grösse
+      baseBrightness: random(100, 255),               // Grundhelligkeit
+      twinkleSpeed: random(0.005, 0.25),              // Funkeln
     });
 }
+
 }
+
+const rotationSpeed = 0.0005; // rad per frame
 
 function draw() {
   background(10,20,80);
 
-    push();
+  
+  //  Polarstern
+  fill(255); // hellgelb
+  circle(originStars.x, originStars.y, 4);
+
 
   // Sternenhimmel
    for (let s of stars) {
     // Funkeln durch Sinus
-    let b = s.baseBrightness + 50 * sin(frameCount * s.twinkleSpeed);
+    push();
+    let b = s.baseBrightness + 50 * sin(frameCount * s.twinkleSpeed); 
+    translate(originStars.x, originStars.y);
+    rotate(frameCount * -rotationSpeed);
+    translate(-originStars.x, -originStars.y);
     fill(b);
+    noStroke()
     circle(s.x, s.y, s.size);
-  }
     pop();
+  }
 
+
+  // Mausposition auf kleineren Bereich mappen
+  let x = map(mouseX, 0, width, 150, 300);  // nur zwischen 150 und 250
+  let y = map(mouseY, 0, height, 150, 300); // nur zwischen 150 und 250
+  
   fill(255,230,150); // hellgelb
-  ellipse(width/2, height/2+100, 200, 200); // Mond 
+  circle(x, y, 150); // Mond
 
   fill(0,0,0); // schwarz
   rect(0, height/3*2, width, height); // Boden
 
-
-// Katze
-
+  // Katze
   push();                
-
   let zoom = size / 100;
   scale(zoom);
   translate(width/2-500,height/3*2-260);  
 
-
- // Schwanz
+  // Schwanz
   stroke(255);
   strokeWeight(18);
   noFill();
   arc(260, 250, 100, 80, 0, PI / 1.5);
 
-  // Fellfarbe
+  // Farbe
   fill(255);
-  noStroke();
   strokeWeight(1);
 
   // Körper
@@ -72,16 +101,24 @@ function draw() {
   triangle(168+40, 128, 185+40+10, 104, 188+40, 128+5);
   triangle(232-40, 128, 215-40-10, 104, 212-40, 128+5);
 
+  stroke(0);
+  let eyeY = 160, d = 30;     // Augenhöhe & Durchmesser
+  let left = 180, right = 220; // Augenpositionen
+  let r = 9;                 // Pupillenbewegungsradius
+
   // Augen
   fill(255);
-  stroke(0);
-  ellipse(180, 165, 25, 20);
-  ellipse(220, 165, 25, 20);
+  ellipse(left, eyeY, d);
+  ellipse(right, eyeY, d);
+
+  // Richtung zur Maus
+  let dx = map(mouseX, 0, width, -r, r);
+  let dy = map(mouseY, 0, height, -r, r);
 
   // Pupillen
   fill(0);
-  ellipse(180, 165, 8, 15);
-  ellipse(220, 165, 8, 15);
+  ellipse(left + dx, eyeY + dy, 8);
+  ellipse(right + dx, eyeY + dy, 8);
 
   // Nase
   fill(255, 150, 150);
@@ -105,7 +142,14 @@ function draw() {
   line(240, 190, 280, 190);
   line(240, 195, 280, 200);
 
-  pop();  
+  pop(); 
+  
+
+  // Fisch folgt  Maus
+  push();
+  translate(mouseX, mouseY); // bewegt die ganze Gruppe
+  drawFish(); // zeichnet den Fisch an der neuen Position
+  pop();
 
 }
 
@@ -113,3 +157,24 @@ function mouseClicked() {
   size += 2; // 
 }
 
+function drawFish() {
+  noStroke()
+
+  // Schwanzflosse hinten
+  fill(255, 120, 0);
+  triangle(-20, 0, -60, -20, -60, 20); // triangel(x1, y1, x2, y2, x3, y3)
+  
+  // Schwanzflosse oben
+  triangle(-30, 0, 0, -40, 30, 10); // triangel(x1, y1, x2, y2, x3, y3)
+
+  // Körper
+
+  fill(255, 150, 0);
+  ellipse(0, 0, 75, 50);
+  
+  // Auge
+  fill(255);
+  circle(18, -10, 15); //circle(x, y, d)
+  fill(0);
+  circle(18, -10, 5);
+}
